84bea6abfd169a351c6ee3192df65ee4d3f05000
Jam <JamX1573@outlook.com>
2024-02-19 11:57:29 +0800

    ffplay (stream_component_open) 代码梳理与注释

--- fftools/ffplay.c
+++ fftools/ffplay.c
@@ -2578,10 +2578,11 @@ static int audio_open(void *opaque, AVChannelLayout *wanted_channel_layout, int 
 }
 
 /* open a given stream. Return 0 if OK */
-static int stream_component_open(VideoState *is, int stream_index)
+static int stream_component_open(VideoState *vs, int stream_index)
 {
-  AVFormatContext *ic = is->ifmt_ctx;
+  AVFormatContext *ifmt_ctx = vs->ifmt_ctx;
   AVCodecContext *avctx;
+  AVStream *av_stream;
   const AVCodec *codec;
   const char *forced_codec_name = NULL;
   AVDictionary *opts = NULL;
@@ -2591,51 +2592,50 @@ static int stream_component_open(VideoState *is, int stream_index)
   int ret = 0;
   int stream_lowres = lowres;
 
-  if (stream_index < 0 || stream_index >= ic->nb_streams)
+  if (stream_index < 0 || stream_index >= ifmt_ctx->nb_streams)
     return -1;
 
   avctx = avcodec_alloc_context3(NULL);
   if (!avctx)
     return AVERROR(ENOMEM);
 
-  ret = avcodec_parameters_to_context(avctx, ic->streams[stream_index]->codecpar);
+  av_stream = ifmt_ctx->streams[stream_index];
+  ret = avcodec_parameters_to_context(avctx, av_stream->codecpar);
   if (ret < 0)
     goto fail;
-  avctx->pkt_timebase = ic->streams[stream_index]->time_base;
+  avctx->pkt_timebase = av_stream->time_base;
 
-  codec = avcodec_find_decoder(avctx->codec_id);
-
+  /* 解码器的创建。如果有指定解码器，根据指定创建；否则根据流信息中的 codec_id 创建 */
   switch (avctx->codec_type) {
   case AVMEDIA_TYPE_AUDIO:
-    is->last_audio_stream = stream_index;
+    vs->last_audio_stream = stream_index;
     forced_codec_name = audio_codec_name;
     break;
   case AVMEDIA_TYPE_SUBTITLE:
-    is->last_subtitle_stream = stream_index;
+    vs->last_subtitle_stream = stream_index;
     forced_codec_name = subtitle_codec_name;
     break;
   case AVMEDIA_TYPE_VIDEO:
-    is->last_video_stream = stream_index;
+    vs->last_video_stream = stream_index;
     forced_codec_name = video_codec_name;
     break;
   }
   if (forced_codec_name)
     codec = avcodec_find_decoder_by_name(forced_codec_name);
+  else
+    codec = avcodec_find_decoder(avctx->codec_id);
   if (!codec) {
     if (forced_codec_name)
-      av_log(NULL, AV_LOG_WARNING,
-             "No codec could be found with name '%s'\n", forced_codec_name);
+      av_log(NULL, AV_LOG_WARNING, "No codec could be found with name '%s'\n", forced_codec_name);
     else
-      av_log(NULL, AV_LOG_WARNING,
-             "No decoder could be found for codec %s\n", avcodec_get_name(avctx->codec_id));
+      av_log(NULL, AV_LOG_WARNING, "No decoder could be found for codec %s\n", avcodec_get_name(avctx->codec_id));
     ret = AVERROR(EINVAL);
     goto fail;
   }
 
   avctx->codec_id = codec->id;
   if (stream_lowres > codec->max_lowres) {
-    av_log(avctx, AV_LOG_WARNING, "The maximum value for lowres supported by the decoder is %d\n",
-           codec->max_lowres);
+    av_log(avctx, AV_LOG_WARNING, "The maximum value for lowres supported by the decoder is %d\n", codec->max_lowres);
     stream_lowres = codec->max_lowres;
   }
   avctx->lowres = stream_lowres;
@@ -2643,8 +2643,8 @@ static int stream_component_open(VideoState *is, int stream_index)
   if (fast)
     avctx->flags2 |= AV_CODEC_FLAG2_FAST;
 
-  ret = filter_codec_opts(codec_opts, avctx->codec_id, ic,
-                          ic->streams[stream_index], codec, &opts);
+  /* 过滤掉给定编解码器的选项。创建一个新的选项字典，其中仅包含适用于ID为codec_id的编解码器的选项。 */
+  ret = filter_codec_opts(codec_opts, avctx->codec_id, ifmt_ctx, av_stream, codec, &opts);
   if (ret < 0)
     goto fail;
 
@@ -2664,73 +2664,77 @@ static int stream_component_open(VideoState *is, int stream_index)
     goto fail;
   }
 
-  is->eof = 0;
-  ic->streams[stream_index]->discard = AVDISCARD_DEFAULT;
+  vs->eof = 0;
+  av_stream->discard = AVDISCARD_DEFAULT;
   switch (avctx->codec_type) {
   case AVMEDIA_TYPE_AUDIO: {
-    AVFilterContext *sink;
+    {
+      AVFilterContext *sink;
 
-    is->audio_filter_src.freq = avctx->sample_rate;
-    ret = av_channel_layout_copy(&is->audio_filter_src.ch_layout, &avctx->ch_layout);
-    if (ret < 0)
-      goto fail;
-    is->audio_filter_src.fmt = avctx->sample_fmt;
-    if ((ret = configure_audio_filters(is, afilters, 0)) < 0)
-      goto fail;
-    sink = is->out_audio_filter;
-    sample_rate = av_buffersink_get_sample_rate(sink);
-    ret = av_buffersink_get_ch_layout(sink, &ch_layout);
-    if (ret < 0)
-      goto fail;
-  }
+      vs->audio_filter_src.freq = avctx->sample_rate;
+      ret = av_channel_layout_copy(&vs->audio_filter_src.ch_layout, &avctx->ch_layout);
+      if (ret < 0)
+        goto fail;
+      vs->audio_filter_src.fmt = avctx->sample_fmt;
+      if ((ret = configure_audio_filters(vs, afilters, 0)) < 0)
+        goto fail;
+      sink = vs->out_audio_filter;
+      sample_rate = av_buffersink_get_sample_rate(sink);
+      ret = av_buffersink_get_ch_layout(sink, &ch_layout);
+      if (ret < 0)
+        goto fail;
+    }
 
     /* prepare audio output */
-    if ((ret = audio_open(is, &ch_layout, sample_rate, &is->audio_tgt)) < 0)
+    if ((ret = audio_open(vs, &ch_layout, sample_rate, &vs->audio_tgt)) < 0)
       goto fail;
-    is->audio_hw_buf_size = ret;
-    is->audio_src = is->audio_tgt;
-    is->audio_buf_size = 0;
-    is->audio_buf_index = 0;
+    vs->audio_hw_buf_size = ret;
+    vs->audio_src = vs->audio_tgt;
+    vs->audio_buf_size = 0;
+    vs->audio_buf_index = 0;
 
     /* init averaging filter */
-    is->audio_diff_avg_coef = exp(log(0.01) / AUDIO_DIFF_AVG_NB);
-    is->audio_diff_avg_count = 0;
+    vs->audio_diff_avg_coef = exp(log(0.01) / AUDIO_DIFF_AVG_NB);
+    vs->audio_diff_avg_count = 0;
     /* since we do not have a precise anough audio FIFO fullness,
        we correct audio sync only if larger than this threshold */
-    is->audio_diff_threshold = (double)(is->audio_hw_buf_size) / is->audio_tgt.bytes_per_sec;
+    vs->audio_diff_threshold = (double)(vs->audio_hw_buf_size) / vs->audio_tgt.bytes_per_sec;
 
-    is->audio_stream = stream_index;
-    is->audio_st = ic->streams[stream_index];
+    vs->audio_stream = stream_index;
+    vs->audio_st = av_stream;
 
-    if ((ret = decoder_init(&is->auddec, avctx, &is->audioq, is->continue_read_thread)) < 0)
+    if ((ret = decoder_init(&vs->auddec, avctx, &vs->audioq, vs->continue_read_thread)) < 0)
       goto fail;
-    if (is->ifmt_ctx->iformat->flags & AVFMT_NOTIMESTAMPS) {
-      is->auddec.start_pts = is->audio_st->start_time;
-      is->auddec.start_pts_tb = is->audio_st->time_base;
+    if (vs->ifmt_ctx->iformat->flags & AVFMT_NOTIMESTAMPS) {
+      vs->auddec.start_pts = vs->audio_st->start_time;
+      vs->auddec.start_pts_tb = vs->audio_st->time_base;
     }
-    if ((ret = decoder_start(&is->auddec, audio_thread, "audio_decoder", is)) < 0)
+    if ((ret = decoder_start(&vs->auddec, audio_thread, "audio_decoder", vs)) < 0)
       goto out;
     SDL_PauseAudioDevice(audio_dev, 0);
     break;
-  case AVMEDIA_TYPE_VIDEO:
-    is->video_stream = stream_index;
-    is->video_st = ic->streams[stream_index];
+  }
+  case AVMEDIA_TYPE_VIDEO: {
+    vs->video_stream = stream_index;
+    vs->video_st = av_stream;
 
-    if ((ret = decoder_init(&is->viddec, avctx, &is->videoq, is->continue_read_thread)) < 0)
+    if ((ret = decoder_init(&vs->viddec, avctx, &vs->videoq, vs->continue_read_thread)) < 0)
       goto fail;
-    if ((ret = decoder_start(&is->viddec, video_thread, "video_decoder", is)) < 0)
+    if ((ret = decoder_start(&vs->viddec, video_thread, "video_decoder", vs)) < 0)
       goto out;
-    is->queue_attachments_req = 1;
+    vs->queue_attachments_req = 1;
     break;
-  case AVMEDIA_TYPE_SUBTITLE:
-    is->subtitle_stream = stream_index;
-    is->subtitle_st = ic->streams[stream_index];
+  }
+  case AVMEDIA_TYPE_SUBTITLE: {
+    vs->subtitle_stream = stream_index;
+    vs->subtitle_st = av_stream;
 
-    if ((ret = decoder_init(&is->subdec, avctx, &is->subtitleq, is->continue_read_thread)) < 0)
+    if ((ret = decoder_init(&vs->subdec, avctx, &vs->subtitleq, vs->continue_read_thread)) < 0)
       goto fail;
-    if ((ret = decoder_start(&is->subdec, subtitle_thread, "subtitle_decoder", is)) < 0)
+    if ((ret = decoder_start(&vs->subdec, subtitle_thread, "subtitle_decoder", vs)) < 0)
       goto out;
     break;
+  }
   default:
     break;
   }
