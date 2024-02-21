208c3d087aa2054aa753e2d8a54d6fab1d47a21a
Jam <JamX1573@outlook.com>
2024-02-21 11:58:41 +0800

    chore(build.sh): 新增 msys2 环境下的编译命令

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ os=$(uname)
 if [ "$os" == "Linux" ]; then
   echo "您正在使用 Linux 操作系统"
   ./configure --prefix=/usr/local/ffmpeg \
-    --disable-static \
     --disable-doc \
     --disable-htmlpages \
     --disable-manpages \
@@ -38,7 +37,6 @@ if [ "$os" == "Linux" ]; then
 elif [ "$os" == "Darwin" ]; then
   echo "您正在使用 macOS 操作系统"
   ./configure --prefix=/usr/local/ffmpeg \
-    --disable-static \
     --disable-doc \
     --disable-htmlpages \
     --disable-manpages \
@@ -68,6 +66,63 @@ elif [ "$os" == "FreeBSD" ]; then
   echo "您正在使用 FreeBSD 操作系统"
 elif [ "$os" == "Windows_NT" ]; then
   echo "您正在使用 Windows 操作系统"
+elif [[ "$os" =~ "MINGW64_NT" ]]; then
+  echo "您正处于 mingw64 环境中"
+  ./configure --prefix=/usr/local/ffmpeg/mingw64 \
+    --disable-doc \
+    --disable-htmlpages \
+    --disable-manpages \
+    --disable-podpages \
+    --disable-txtpages \
+    --disable-static \
+    --enable-shared \
+    --enable-gpl \
+    --enable-version3 \
+    --enable-nonfree \
+    --enable-postproc \
+    --enable-libass \
+    --enable-libfdk-aac \
+    --enable-libfreetype \
+    --enable-libmp3lame \
+    --enable-libopenjpeg \
+    --enable-openssl \
+    --enable-libopus \
+    --enable-libspeex \
+    --enable-libtheora \
+    --enable-libvorbis \
+    --enable-libvpx \
+    --enable-libxvid \
+    --enable-vaapi \
+    --enable-libx264 \
+    --enable-libx265
+elif [[ "$os" =~ "MSYS_NT" ]]; then
+  echo "您正处于 msys2 环境中"
+  ./configure --prefix=/usr/local/ffmpeg/msvc \
+    --disable-doc \
+    --disable-htmlpages \
+    --disable-manpages \
+    --disable-podpages \
+    --disable-txtpages \
+    --disable-static \
+    --enable-shared \
+    --enable-gpl \
+    --enable-version3 \
+    --enable-nonfree \
+    --toolchain=msvc
+  # ./configure --prefix=/usr/local/ffmpeg/msvc \
+  #   --disable-doc \
+  #   --disable-htmlpages \
+  #   --disable-manpages \
+  #   --disable-podpages \
+  #   --disable-txtpages \
+  #   --disable-static \
+  #   --enable-shared \
+  #   --enable-gpl \
+  #   --enable-version3 \
+  #   --enable-nonfree \
+  #   --enable-libx264 \
+  #   --enable-libx265 \
+  #   --toolchain=msvc
 else
   echo "未知的操作系统: $os"
 fi
